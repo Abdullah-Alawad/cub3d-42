ef8a042bcb683dd0d7b3d42650e4130d569f8383
Maryam Odat <modat@c1r5s9.42amman.com>
2025-09-17 12:18:55 +0300

    big map added

--- srcs/yakuza.c
+++ srcs/yakuza.c
@@ -6,7 +6,7 @@
 /*   By: modat <modat@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/09/02 11:29:28 by modat             #+#    #+#             */
-/*   Updated: 2025/09/13 16:22:13 by modat            ###   ########.fr       */
+/*   Updated: 2025/09/17 09:32:44 by modat            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,6 @@ TODO :
 - it is fine to do a little parsing.
 
 */
-// void    load_textures(t_wall **news)
-// {
-//     (*news) = malloc(sizeof(t_wall));
-//     if (!*news)
-//     {
-//         // free
-//         exit(0);
-//     }
-//     (*news)->north = mlx_load_xpm42("./wall.xpm42");
-//     if (!(*news)->north)
-//         printf("path error\n");
-//     (*news)->south = mlx_load_xpm42("texture/wall.xpm42");
-//     (*news)->east = mlx_load_xpm42("texture/wall_window.xpm42");
-//     (*news)->west = mlx_load_xpm42("texture/wall.xpm42");
-// }
 
 void	 setup_config(t_tokugawa_sokoku **yakuza, char **av, int ac)
 {
@@ -61,6 +46,57 @@ void	draw_background(t_minimap *minimap)
 	}
 }
 
+
+
+static void 	map_drawing(t_tokugawa_sokoku *yakuza, uint32_t color, int pw, int ph)
+{
+    int w;
+    int h;
+    int wCopy = yakuza->map->width;
+    int hCopy = yakuza->map->height;
+    h = 0;
+    while (h < ph)
+    {
+        wCopy = yakuza->map->width;
+        w = 0;
+        while (w < pw)
+        {
+			mlx_put_pixel(yakuza->img, wCopy, hCopy, color);
+            w++;
+            wCopy++;
+        }
+        h++;
+        hCopy++;
+    }
+}
+
+void 	map(t_tokugawa_sokoku *yakuza)
+{
+	int h = 0;
+	int w;
+	int ph = HEIGHT / yakuza->map->height;
+	int pw = WIDTH / yakuza->map->width;
+	yakuza->map->height = 0;
+	while (yakuza->map->map[h])
+	{
+		yakuza->map->width = 0;
+		w = 0;
+		while (yakuza->map->map[h][w])
+		{
+			if (yakuza->map->map[h][w] == '1')
+                map_drawing(yakuza, 0xBBDDFBBB, pw, ph);
+			if (yakuza->map->map[h][w] == 'N')
+                map_drawing(yakuza, 0xBBDAABBB, pw, ph);
+			w++;
+			yakuza->map->width += pw;
+		}
+		yakuza->map->height += ph;
+		h++;
+	}
+}
+
+
+
 int	main(int ac, char **av)
 {
 	t_tokugawa_sokoku *yakuza;
@@ -68,6 +104,8 @@ int	main(int ac, char **av)
 	setup_config(&yakuza, av, ac);
 	draw_background(yakuza->minimap);
 	minimap(yakuza->minimap, yakuza->map);
+	map(yakuza);
+	// map_drawing(yakuza);
 	// xy ???
 	// mouse_hook(yakuza->mouse, yakuza, yakuza->mlx);
 
